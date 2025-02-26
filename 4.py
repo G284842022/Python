aefa835024a59c443baadb3687acc733f34d79ab
G284842022 <228484@st.takushoku-u.ac.jp>
2025-02-26 11:52:50 +0900

    2

--- 4.py
+++ 4.py
@@ -2,8 +2,8 @@ def numbers(n):
     for i in range(2, n+1):
         for j in range(1,i):
             print(j, end="")
-        for j in range(1,i-1):
-            print(i-j-1, end="")
+        for j in range(i - 2, 0, -1):
+            print(j, end="")
         print()
 
 numbers(5)
